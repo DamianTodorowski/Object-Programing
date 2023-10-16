9ffb47cd0bc55ac17bea62d20299cd226594c10c
DamianTodorowski <115210660+DamianTodorowski@users.noreply.github.com>
2023-10-16 16:15:29 +0200

    update

--- Lab_1/F1/src/F1.java
+++ Lab_1/F1/src/F1.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class F1 {
     public static void main(String[] args) {
         int tab1[]={3,2,-10,54,-8,2,12,89,3,-21};
@@ -5,9 +7,9 @@ public class F1 {
         {
             System.out.println(tab1[i]);
         }
-        int tab2[]=new int[10];
+        int tab2[]=new int[11];
         Random random = new Random();
-        random =setSeed(345);
+        random = random;
         for(int i =tab2.length;i>=0;i--)
         {
             System.out.println(tab2[i]);
