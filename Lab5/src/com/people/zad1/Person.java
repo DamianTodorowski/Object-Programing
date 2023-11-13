751b33d074034a8bfc5ee36b650881be779612a6
DamianTodorowski <115210660+DamianTodorowski@users.noreply.github.com>
2023-11-13 15:24:33 +0100

    update

--- Lab5/src/com/people/zad1/Person.java
+++ Lab5/src/com/people/zad1/Person.java
@@ -2,14 +2,16 @@ package com.people.zad1;
 
 class Person {
 
-    private static int counter;
+    private static int counter=0;
     private String name;
 
     public Person(){
+
         counter++;
     }
 
     public static int getCounter() {
+
         return counter;
     }
 }
