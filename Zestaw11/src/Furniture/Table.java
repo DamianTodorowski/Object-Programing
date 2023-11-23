6b1359f9210de27dce123612ceb60c49487dbc62
DamianTodorowski <115210660+DamianTodorowski@users.noreply.github.com>
2023-11-23 22:40:05 +0100

    Furniture update

--- Zestaw11/src/Furniture/Table.java
+++ Zestaw11/src/Furniture/Table.java
@@ -6,6 +6,9 @@ package Furniture;
     private Table(double price){
         this.price=price;
     }
+    static Table createTable(double price){
+        return new Table(price);
+    }
 
     public double getPrice() {
         return price;
